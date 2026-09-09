e077132f8f2bebcd5e60146ecc1563ee00f77d22
faceontable <faceontable.ro@gmail.com>
2026-09-09 15:15:27 +0300

    Добавил eslint

--- src/main.js
+++ src/main.js
@@ -1,24 +1,41 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { object, string } from 'yup';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const state = {
+  form: {
+    url: ''
+  }
+}
 
-setupCounter(document.querySelector('#counter'))
+let schema = object({
+  url: string().url().required()
+})
+
+const renderSearchForm = () => {
+  const form = document.querySelector('form')
+
+  const search = document.querySelector('input')
+  form.appendChild(search)
+
+  const button = document.querySelector('button')
+  button.textContent = 'Добавить'
+  form.appendChild(button)
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const formData = new FormData(form)
+    const url = formData.get('url')
+    schema.validate({ url })
+      .then(() => {
+        state.form.url = url;
+        console.log('Valid URL:', state.form.url);
+      })
+      .catch((err) => {
+        console.error('Validation error:', err.errors);
+      });
+  })
+
+  return form
+}
+
+document.querySelector('#app').append(renderSearchForm())
